9c23df6200b0cac83e29a2623c0c70d0f14207a6
hamael <hamael18@hotmail.com>
2019-11-08 13:34:24 +0100

    JS like sur toutes les pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 require('../css/app.css');
 require('../css/animated_css.css');
 require('jquery/dist/jquery');
+require ('axios/dist/axios.min');
+
 global.$ = global.jQuery = $;
+global.axios = axios;
 
 const element =  document.querySelector('#flashes');
 element.classList.add('animated', 'bounceOut', 'delay-2s');
